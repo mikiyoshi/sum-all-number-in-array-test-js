a3c48b18cb08c596cff8f8d5add128254f3ed63e
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-01-17 05:29:57 +0000

    add function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  const flat = array.flat(Infinity);
+  let sum = 0;
+  for (let i = 0; i < flat.length; i++) {
+    sum = sum + flat[i];
+  }
+  return sum;
 }
 
 module.exports = sumItems;
